e34cb03a97bcb1def28a90a503468757c68f81b1
Md. Soyeb Islam <soyeb10191@gmail.com>
2025-03-01 03:02:33 +0600

    add bg changer functionality

--- js/button.js
+++ js/button.js
@@ -7,6 +7,12 @@ const completedTasksElement = document.getElementById('completed-task');
 const titleEl = document.getElementById('title');
 const newListEl = document.getElementById('new-list');
 
+// Get current time
+const date = new Date();
+const timeString = date.toLocaleString();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
 
 const completeBtn = document.querySelectorAll('#complete-btn');
 
@@ -14,11 +20,13 @@ const completeBtn = document.querySelectorAll('#complete-btn');
 
 for (let i = 0; i < completeBtn.length; i++) {
     completeBtn[i].addEventListener('click', function(event) {
-        const titleText = titleEl.textContent;
+        
+        // data attribute
+        const titleText = event.target.getAttribute('data-title');
 
         const listItem = document.createElement('li');
 
-        listItem.innerHTML = titleText;
+        listItem.innerHTML = titleText + ' - at ' + timeString;
 
         newListEl.appendChild(listItem);
 
@@ -43,6 +51,35 @@ for (let i = 0; i < completeBtn.length; i++) {
         } else {
             alert('There is no tasks remain!');}
     })
+
+    // History clear button
+    const clearBtn = document.getElementById('clear-btn');
+    clearBtn.addEventListener('click', function() {
+        newListEl.innerHTML = '';
+    })
+
+    // Theme changing button
+
+    const randomColor = function () {
+        const hex = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+          color += hex[Math.floor(Math.random() * 16)];
+        }
+        return color;
+      };
+    
+      // Event listener to change the background color when button is clicked
+      document.querySelector('#bg-change').addEventListener('click', function() {
+        document.body.style.backgroundColor = randomColor();
+      });
+
+
+  
+  
+  
+
+
 }
 
 
