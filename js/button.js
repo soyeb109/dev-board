f59c6fe44c0ae07417d85bca21f6c9823495e267
Md. Soyeb Islam <soyeb10191@gmail.com>
2025-03-01 02:06:03 +0600

    Add title to history list

--- js/button.js
+++ js/button.js
@@ -4,12 +4,25 @@ let completedTasks = 0;
 const remainingTasksElement = document.getElementById('remaining-task');
 const completedTasksElement = document.getElementById('completed-task');
 
+const titleEl = document.getElementById('title');
+const newListEl = document.getElementById('new-list');
+
+
 const completeBtn = document.querySelectorAll('#complete-btn');
 
 
 
 for (let i = 0; i < completeBtn.length; i++) {
     completeBtn[i].addEventListener('click', function(event) {
+        const titleText = titleEl.textContent;
+
+        const listItem = document.createElement('li');
+
+        listItem.innerHTML = titleText;
+
+        newListEl.appendChild(listItem);
+
+
         if(remainingTasks >= 0) {
             remainingTasks--;
             completedTasks++;
@@ -22,6 +35,7 @@ for (let i = 0; i < completeBtn.length; i++) {
             event.target.disabled = true;
             event.target.style.backgroundColor = 'grey';
 
+           
             if (remainingTasks === 0) {
                 alert('Congrats! you have completed all the tasks');
             }
@@ -30,3 +44,6 @@ for (let i = 0; i < completeBtn.length; i++) {
             alert('There is no tasks remain!');}
     })
 }
+
+
+
